4a251366987b0bdfeaedc2ca4a0bb61f7559ddce
Amar <ampit.xd@gmail.com>
2021-06-17 23:55:51 -0400

    Pagination

--- src/App.js
+++ src/App.js
@@ -8,29 +8,17 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [fetching, setFetching] = useState(false);
   const [numBooks, setNumBooks] = useState(0);
+  const [numPages, setNumPages] = useState(0);
   const [books, setBooks] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // fetch data from api and populate results
+    setFetching(true);
     try {
       const { data } = await axios.get(SEARCH_URL + searchTerm);
       setNumBooks(data.numFound);
-      setFetching(true);
-      const numPages = Math.ceil(data.numFound / 100);
-      const requests = [];
-      let books = data.docs;
-      setBooks(books);
-      // create all requests.
-      for (let i = 2; i <= numPages; i++) {
-        requests.push(axios.get(SEARCH_URL + searchTerm + "&page=" + i));
-      }
-      // concurrent requests
-      await Promise.all(requests).then((results) => {
-        results.forEach((result) => books.push(...result.data.docs));
-      });
-      setBooks(books);
-      console.log(books);
+      setNumPages(Math.ceil(data.numFound / 100));
+      setBooks(data.docs);
       setFetching(false);
     } catch (error) {
       console.error(error);
@@ -38,8 +26,8 @@ function App() {
   };
 
   const sortTitle = () => {
-    console.log("clicked");
-    let sorted = books.sort((a, b) => {
+    const booksToSort = books.slice();
+    booksToSort.sort((a, b) => {
       const nameA = a.title.toUpperCase(); // ignore upper and lowercase
       const nameB = b.title.toUpperCase(); // ignore upper and lowercase
       if (nameA < nameB) {
@@ -50,7 +38,45 @@ function App() {
       }
       return 0; // names must be equal
     });
-    setBooks(sorted);
+    setBooks(booksToSort);
+  };
+
+  const updatePage = async (page) => {
+    setFetching(true);
+    try {
+      const { data } = await axios.get(page);
+      setBooks(data.docs);
+      setFetching(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const pagination = () => {
+    const pages = [];
+    for (let i = 2; i <= numPages; i++) {
+      pages.push(SEARCH_URL + searchTerm + "&page=" + i);
+    }
+    return (
+      <>
+        <h5 className="navPageTitle">Pages</h5>
+        <ul className="tableNav">
+          {pages.map((page, index) => (
+            <li>
+              <button
+                className="tableBtn"
+                onClick={() => {
+                  updatePage(page);
+                  window.scrollTo(0, 0);
+                }}
+              >
+                {index + 2}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
   };
 
   return (
@@ -67,16 +93,17 @@ function App() {
         <input type="submit" value="Search" className="submitBtn" />
       </form>
       {/* Show Results in a table here (Title, Book Cover, Author, Publish Date) */}
-      {fetching && (
-        <div className="fetching">Fetching all {numBooks} books...</div>
-      )}
+      {fetching && <div className="fetching">Fetching...</div>}
+      {numBooks && <div className="booksNumber">{numBooks} Books Found.</div>}
       <table className="table">
         <tbody>
           <tr>
-            <th onClick={sortTitle}>Title</th>
+            <th className="sortField" onClick={() => sortTitle()}>
+              Title
+            </th>
             <th>Book Cover</th>
             <th>Author</th>
-            <th>Publish Date</th>
+            <th className="sortField">Publish Date</th>
           </tr>
           {books &&
             books.map((row, index) => (
@@ -103,6 +130,8 @@ function App() {
             ))}
         </tbody>
       </table>
+      {/* Pagination */}
+      {pagination()}
     </div>
   );
 }
